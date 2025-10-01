7990cc88dc784b3e6fe6ff17c9fddc16ed894016
ATKPowder <67365703+jotica0602@users.noreply.github.com>
2025-10-01 00:41:09 -0400

    Update list_files_dialog.py

--- gui/list_files_dialog.py
+++ gui/list_files_dialog.py
@@ -8,7 +8,7 @@ class ListFilesDialog(tk.Toplevel):
         self.result = None
 
         # Etiquetas
-        tk.Label(self, text="Etiquetas (separadas por espacios)\nDejar vacío para mostrar todos").grid(row=0, column=0, sticky="w", padx=5, pady=5)
+        tk.Label(self, text="Etiquetas (separadas por coma)\nDejar vacío para mostrar todos").grid(row=0, column=0, sticky="w", padx=5, pady=5)
         self.tags_entry = tk.Entry(self, width=40)
         self.tags_entry.grid(row=0, column=1, columnspan=2, padx=5, pady=5)
 
@@ -19,11 +19,9 @@ class ListFilesDialog(tk.Toplevel):
         self.grab_set()  # modal
 
     def on_ok(self):
-        tags = self.tags_entry.get().strip().split()
-        print(f"TODOS LOS TAGS: {tags}")
-
-        if tags:
-            self.result = tags
+        tags = self.tags_entry.get().strip()
+        if len(tags):
+            self.result = tags.split(",")
         else:
             self.result = []  # vacío → mostrar todos los archivos
 
